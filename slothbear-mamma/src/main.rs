f6d6ae02862a4bd1944fae5581e31d62b02ae3ad
Pocket of Weird <hartzlernathan@gmail.com>
2020-04-04 14:12:59 -0500

    Renaming api service and moving slothbear-mamma

--- slothbear-mamma/src/main.rs
+++ slothbear-mamma/src/main.rs
@@ -1,30 +1,3 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-#[macro_use] 
-extern crate rocket;
-extern crate rocket_contrib;
-#[macro_use]
-extern crate rocket_okapi;
-extern crate rand;
-extern crate serde;
-extern crate schemars;
-
-#[cfg(test)] 
-mod tests;
-
-mod models;
-mod routes;
-mod service;
-
-
 fn main() {
-    rocket::ignite()
-    .mount( 
-        "/slothbear", 
-        routes_with_openapi![
-                routes::index, 
-                routes::post_render,
-            ]
-    )
-    .launch();
+    println!("Hello, world!");
 }
